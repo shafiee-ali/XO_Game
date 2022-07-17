a043c9de750faaa0908fe32c9b0a577911b212d9
Ali Shafiee <shafiee.ali10@gmail.com>
2022-07-17 09:47:08 +0430

    Add xo_board class to project

--- XO/Entities/xo_board.py
+++ XO/Entities/xo_board.py
@@ -1,42 +1,10 @@
-from Entities.xo_board import XOBoard
-from Entities.player import Player
+from board import Board
 
 
-class XOGame:
-    def __init__(self, n: int, m: int):
-        """
-        :param n: Board size
-        :param m: Number of players
-        """
-        if n == 0:
-            raise Exception("wrong board size")
-        if m == 0:
-            raise Exception("wrong player numbers")
+class XOBoard(Board):
+    def __init__(self, n):
+        super(XOBoard, self).__init__(n)
+        self.chain_size = 3
 
-        self.players_count = m
-        self.board = XOBoard(n)
-        self.players = [Player(i) for i in range(self.players_count)]
-        self.curr_player_id = 0
-
-    def action(self, x: int, y: int):
-        """
-
-        :param x:
-        :param y:
-        :return:
-        """
-        self.board.fill_cell(x, y, self.curr_player_id)
-        self.next_player()
-
-
-    def next_player(self):
-        """
-
-        :return:
-        """
-        self.curr_player_id = (self.curr_player_id + 1) % self.players
-
-    def is_end_game(self):
-        return self.board.
-
-    def next(self, ):
+    def is_matched(self):
+        pass
